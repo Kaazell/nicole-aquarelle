2b473724982a47522cfe058381ff0ec7d2053639
Atef Lachhab <atef.lachhab1@gmail.com>
2024-12-03 15:46:56 -0500

    feat: home page about section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import s from "./style.module.scss";
 import Button from "../../components/Button/Button";
+import nicole from "../../assets/img/nicole-tremblay.png"
 
 const Home: React.FC = () => {
   return (
     <main>
       <section className={s.landing} id="home_section">
         <div className={s.landing__center}>
-        <h1 className={s.landing__title}>Nicole Tremblay</h1>
-        <p className={s.landing__introduction}>Bienvenue dans mon univers</p>
+          <h1 className={s.landing__title}>Nicole Tremblay</h1>
+          <p className={s.landing__introduction}>Bienvenue dans l'univers onirique de l'aquarelle.</p>
         </div>
-        <Button buttonName="Contactez-moi" destination="#contact_form" />
+        <Button buttonName="Decouvrir la galerie" destination="#contact_form" />
+      </section>
+      <section className={s.artist}>
+        <div className={s.artist__photo}>
+          <img src={nicole} alt="Photo de l'artiste" />
+        </div>
+        <div className={s.artist__bio}>
+          <h2>À propos de l'artiste</h2>
+          <p>L'artiste explore l'aquarelle avec une passion profonde, utilisant la fluidité de l'eau pour créer des œuvres vibrantes. Chaque peinture reflète son amour pour les nuances subtiles et les jeux de lumière. À travers ses pinceaux, elle transmet des émotions, invitant à une immersion poétique dans des paysages fascinants.</p>
+        </div>
       </section>
     </main>
   );
