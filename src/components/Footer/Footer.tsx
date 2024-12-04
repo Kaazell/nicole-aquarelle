46b0000aed0af817eebd20fe0f88cf68a51d99de
Atef Lachhab <atef.lachhab1@gmail.com>
2024-12-04 16:39:15 -0500

    feat: footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,20 +2,18 @@ import s from "./style.module.scss";
 
 export function Footer() {
   return (
-    <footer className={s.footer}>
+    <footer>
+      <div className={s.footer}>
       <div className={s.footer__left}>
         <h2 className={s.footer__text}>Nicole Tremblay</h2>
         <p className={s.footer__text}>
-          Copyright © <span id="current-year">2024</span>
+          +1 514 555 5555
         </p>
       </div>
       <div className={s.footer__left}>
-        <p className={s.footer__text}>Nicole Tremblay</p>
-        <p className={s.footer__text}>
-          Copyright © <span id="current-year">2024</span>
-        </p>
+        <p className={s.reboot}>Reboot Web<span className={s.red}>_</span></p>
+      </div>
       </div>
     </footer>
-    
   );
 }
