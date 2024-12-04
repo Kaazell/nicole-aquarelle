2ae000d1c2fd251638c052bd215ce1a33d207a64
Atef Lachhab <atef.lachhab1@gmail.com>
2024-12-04 17:07:30 -0500

    test2

---
+++ global.d.ts
@@ -0,0 +1,10 @@
+declare module 'react-burger-menu' {
+    const content: any;
+    export default content;
+  }
+  
+  declare module 'react-router-hash-link' {
+    const content: any;
+    export default content;
+  }
+  
